0bda515a97f1edb1b9bda1a6e43fd9b01951cbac
Hassan Tijani <gatukurh1@mail>
2023-01-23 08:33:19 +0100

    Hassan Setup

---
+++ js/general.js
@@ -0,0 +1,25 @@
+
+(function(){
+    let general = {
+        init: function(){
+            
+        },
+    
+        register: function(params) {
+            
+        },
+    
+        mailSystem: function(){
+
+        },
+
+        notificationSystem: function(){
+            
+        }
+    }
+
+    $(document).ready(function () {
+        camp_pipe.init();
+    });
+
+})();
